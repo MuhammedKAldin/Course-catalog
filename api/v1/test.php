f65c3745693f8be0911b53201e0fde49d038bfa3
substanceshift <muhammedk.aldin@gmail.com>
2024-10-04 02:56:49 +0300

    initial commit

---
+++ api/v1/test.php
@@ -0,0 +1,11 @@
+<?php
+include_once 'config/database.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
+if ($db) {
+    echo "Database connection successful!";
+} else {
+    echo "Failed to connect to the database.";
+}
